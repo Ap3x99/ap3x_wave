5002c39ba78f3bdca8698befe9fdaabcdd55e964
Ap3x99 <74175693+Ap3x99@users.noreply.github.com>
2021-09-02 15:28:28 -0500

    Small update
    Quick changes.

--- sv.lua
+++ sv.lua
@@ -8,16 +8,15 @@ local label =
  /_/  |_/_/   /____/_/|_|     |__/|__/_/  |_|___/_____/   
                                                        
 					  Created by Ap3x99
+					Script is up to date!
 ]]
-
-print( label )
 
 --version checker
 Citizen.CreateThread( function()
 	updatePath = "/Ap3x99/ap3x_wave"
 	resourceName = "ap3x_wave ("..GetCurrentResourceName()..")"
 	
-	function checkVersion(err,responseText, headers)
+	function checkVersion(err, responseText, headers)
 		curVersion = LoadResourceFile(GetCurrentResourceName(), "version") -- make sure the "version" file actually exists in your resource root!
 	
 		if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
@@ -27,7 +26,7 @@ Citizen.CreateThread( function()
 		elseif tonumber(curVersion) > tonumber(responseText) then
 			print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online i advise you to update ( or downgrade? )")
 		else
-			print("\n"..resourceName.." is up to date!")
+			print( label )
 		end
 	end
 	
